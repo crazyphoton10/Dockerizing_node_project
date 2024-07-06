2b6c708e24952c5cef06711d5a9897ba4994b9c3
Kumar Shantanu <thecrazyphoton@gmail.com>
2024-07-07 02:31:51 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+const dotenv = require("dotenv");
 const app = express();
+dotenv.config();
 
 app.get("/home", (req, res) => {
   res.json({ msg: "OK" });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Started the server");
 });
